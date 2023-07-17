5bdd0e78057a05cfe26eb1a41ec1973917154595
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-07-18 01:25:29 +0600

    assignment project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import HeaderTop from './components/HeaderTop.vue'
+  import BannerTop from './components/BannerTop.vue'
+</script>
+
+<template>
+ <HeaderTop/>
+ <BannerTop/>
+
+</template>
+
+<style scoped>
+@import '/src/assets/bootstrap.min.css';
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import '/src/assets/style.css'
+import '/src/assets/vendor/aos/aos.css'
+import '/src/assets/bootstrap.min.css'
+import '/src/assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import '/src/assets/vendor/boxicons/css/boxicons.min.css'
+import '/src/assets/vendor/swiper/swiper-bundle.min.css'
+
+
+import '/src/assets/bootstrap.bundle.min.js'
+import '/src/assets/vendor/swiper/swiper-bundle.min.js'
+import '/src/assets/js/main.js'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
